28d29b3acffc947840bad001f41010fb0610dcd2
Matt Currie <mslurr5@gmail.com>
2018-01-27 00:52:58 -0500

    Added remote password changing utility for Mac. Untested and uncommented, but it should work.

---
+++ remotepasswd.sh
@@ -0,0 +1,27 @@
+### WORK IN PROGRESS ###
+ADMIN="administrator"
+
+function user_select {
+    USERS=( $(ls /Users/ | grep -v 'Shared') )
+
+    USER_COUNTER=0
+    for i in ${USERS[@]};
+    do
+        let "USER_COUNTER++"
+        echo "$USER_COUNTER: $i";
+    done
+
+    read -p "Type the user number: " no
+
+    if [ "$no" -ge 1 -a "$no" -le "$USER_COUNTER" ]; then
+        USER_SELECTED=${USERS[$no-1]}
+    else
+        echo "Please choose a number that correlates to a user."
+        user_select
+    fi
+}
+read -p "Enter the computer you wish to configure: " $PC
+ssh administrator@$PC
+echo "Which user would you like to change the password for?"
+user_select
+sudo passwd $USER_SELECTED
